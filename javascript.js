c167f76db9ff964c42827428f000921e58edaabd
Eric <ericfaiscaextremo@gmail.com>
2022-05-03 23:32:09 -0300

    finalizado processo de retirar

--- javascript.js
+++ javascript.js
@@ -44,25 +44,25 @@ function Comprar() {
 };
 
 function Retirar() {
-    if ( (document.getElementById("selectQueijo").checked) || (quantidadeQueijo > 0) || (valoresQueijo > 0) ) {
+    if ( (document.getElementById("selectQueijo").checked) && (quantidadeQueijo > 0) && (valoresQueijo > 0) ) {
         quantidadeQueijo = quantidadeQueijo - 1;
         valoresQueijo = valoresQueijo - 30.00;
         document.getElementById("quantidadeQueijo").innerHTML = quantidadeQueijo;
         document.getElementById("valoresQueijo").innerHTML = valoresQueijo;
 
-    } else if ( (document.getElementById("selectFrango").checked) || (quantidadeFrango > 0) || (valoresFrango > 0) )  {
+    } else if ( (document.getElementById("selectFrango").checked) && (quantidadeFrango > 0) && (valoresFrango > 0) )  {
         quantidadeFrango = quantidadeFrango - 1;
         valoresFrango = valoresFrango - 50.00;
         document.getElementById("quantidadeFrango").innerHTML = quantidadeFrango;
         document.getElementById("valoresFrango").innerHTML = valoresFrango;
 
-    } else if ( (document.getElementById("selectPeperone").checked) || (quantidadePeperone > 0) || (valoresPeperone > 0) ) {
+    } else if ( (document.getElementById("selectPeperone").checked) && (quantidadePeperone > 0) && (valoresPeperone > 0) ) {
         quantidadePeperone = quantidadePeperone - 1;
         valoresPeperone = valoresPeperone - 20.00;
         document.getElementById("quantidadePeperone").innerHTML = quantidadePeperone;
         document.getElementById("valoresPeperone").innerHTML = valoresPeperone;
 
-    } else if ( (document.getElementById("selectBrocolis").checked) || (quantidadeBrocolis > 0) || (valoresBrocolis > 0) ) {
+    } else if ( (document.getElementById("selectBrocolis").checked) && (quantidadeBrocolis > 0) && (valoresBrocolis > 0) ) {
         quantidadeBrocolis = quantidadeBrocolis - 1;
         valoresBrocolis = valoresBrocolis - 25.00;
         document.getElementById("quantidadeBrocolis").innerHTML = quantidadeBrocolis;
